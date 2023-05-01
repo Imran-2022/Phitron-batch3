4d62720525ef7385c19f53597fd81af88dbd6696
Imran-2022 <mdimranulhaque202@gmail.com>
2023-05-01 09:26:56 +0600

    m-14

--- Introduction to Programming Language & Problem Solving Part-I/0.code/code.c
+++ Introduction to Programming Language & Problem Solving Part-I/0.code/code.c
@@ -1,25 +1,10 @@
-#include <stdio.h>
-#include <string.h>
-
-int main()
-{
-    char ch1[11];
-    char ch2[11];
-    scanf("%s%s", &ch1, &ch2);
-    int len1 = strlen(ch1);
-    int len2 = strlen(ch2);
-
-    printf("%d %d\n", len1, len2);
-    
-    char A[11];
-    strcpy(A, ch1);
-    printf("%s\n", strcat(A, ch2));
-
-    char temp = ch1[0];
-    ch1[0] = ch2[0];
-    ch2[0] = temp;
-
-    printf("%s %s", ch1, ch2);
-
-    return 0;
-}
+ #include <stdio.h>
+    int sum(int x, int y)
+    {
+    	int res = x+y;
+        return res;
+    }
+    int main()
+    {
+        sum(5,6);
+    }
