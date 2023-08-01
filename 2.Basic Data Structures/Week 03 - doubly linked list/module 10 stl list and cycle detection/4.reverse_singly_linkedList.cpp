b00cc2d1352d5593462a2156db533f5618e1cc2e
Imran-2022 <mdimranulhaque202@gmail.com>
2023-08-01 10:00:10 +0600

    update

---
+++ 2.Basic Data Structures/Week 03 - doubly linked list/module 10 stl list and cycle detection/4.reverse_singly_linkedList.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    tail = newNode;
+}
+
+void reverse(Node *&head, Node *current)
+{
+    if (current->next == NULL)
+    {
+        head = current;
+        return;
+    }
+    reverse(head, current->next);
+    current->next->next = current;
+    current->next = NULL;
+}
+
+void print(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+    while (true)
+    {
+        int x;
+        cin >> x;
+        if (x == -1)
+            break;
+        insert(head, tail, x);
+    }
+    reverse(head, head);
+    print(head);
+
+    return 0;
+}
---
+++ 2.Basic Data Structures/Week 03 - doubly linked list/module 10 stl list and cycle detection/5.reverse_doubly_linked_list.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *pre;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->pre = NULL;
+    }
+};
+
+void insert(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    newNode->pre = tail;
+    tail = newNode;
+}
+
+void print(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+}
+
+void reverse(Node*head,Node*tail){
+    Node *i=head;
+    Node*j=tail;
+    while(i!=j && i->next!=j){
+        swap(i->val,j->val);
+        i=i->next;
+        j=j->pre;
+    }
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+    while (true)
+    {
+        int x;
+        cin >> x;
+        if (x == -1)
+            break;
+        insert(head, tail, x);
+    }
+    reverse(head,tail);
+    print(head);
+    return 0;
+}
