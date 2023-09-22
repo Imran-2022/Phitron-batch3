9ef97c90f486bfd2fa33965943b0c193e5b31fa9
Imran-2022 <mdimranulhaque202@gmail.com>
2023-09-22 17:52:57 +0600

    updt

---
+++ xpscContest/1.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    cout<<"hello world !"<<endl;
+}
+
+int main()
+{
+    int t=1;
+    cin>>t;
+    while(t--)solve();
+    return 0;
+}
