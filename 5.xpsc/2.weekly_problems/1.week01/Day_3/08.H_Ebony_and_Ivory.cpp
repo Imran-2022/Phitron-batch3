d32fa7fcc45a8693303ea6ebcc679c04439ad2ca
Imran-2022 <mdimranulhaque202@gmail.com>
2023-10-02 20:19:12 +0600

    updt

--- 5.xpsc/2.weekly_problems/1.week01/Day_3/08.H_Ebony_and_Ivory.cpp
+++ 5.xpsc/2.weekly_problems/1.week01/Day_3/08.H_Ebony_and_Ivory.cpp
@@ -1,6 +1,33 @@
 /**
  * problem link : https://codeforces.com/problemset/problem/633/A
  * 
- * new to me : 
+ * new to me : learn new technique wow.. !
  * 
 */
+
+#include<bits/stdc++.h>
+#include<stdio.h>
+using namespace std;
+
+
+#define ll 	 long long
+#define fr(i,n)           for(int i=0;i<n;i++)
+#define fr1(i,n)         for(ll i=1;i<=n;i++)
+int main()
+{
+    ll m,n,k,i,j,ans,d;
+    while(cin>>m>>n>>k)
+    {
+        ans=0;
+        fr(i,1000)
+        {
+            d=(k-(m*i));
+            if(d<0){ans=0;break;}
+            if (d%n==0 ) { ans=1;break;}
+        }
+
+        if(ans)cout<<"Yes"<<endl;
+        else cout<<"No"<<endl;
+    }
+
+}
