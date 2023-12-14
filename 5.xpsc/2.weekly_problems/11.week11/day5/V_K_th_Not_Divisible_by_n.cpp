d41fa36d55516524719ae00f8aafaca7b908de96
Imran-2022 <mdimranulhaque202@gmail.com>
2023-12-14 16:43:06 +0600

    updt

---
+++ 5.xpsc/2.weekly_problems/11.week11/day5/V_K_th_Not_Divisible_by_n.cpp
@@ -0,0 +1,40 @@
+/**
+* author : _Grandmaster
+* created : 14-12-2023 16:40:41
+**/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define newline cout << endl;
+#define e '\n'
+#define fl(i, a, n) for (int i = a; i < n; i++)
+#define F first
+#define S second
+#define pb push_back
+#define all(p) p.begin(), p.end()
+#define rall(v) v.rbegin(), v.rend()
+#define si set<int>
+#define vi vector<int>
+#define vvi vector<vi>
+#define pii pair<int, int>
+#define print(a)for (auto x : a)cout << x << ' ';cout<<e;
+#define print1(a)for (auto x : a)cout << x.F << ' ' << x.S << endl;
+#define IMRAN ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+using namespace std;
+
+void solve(){
+    int n,k,i;
+    cin>>n>>k; 
+	cout<<(k+((k-1)/(n-1)))<<endl;
+}
+
+int main()
+{
+    IMRAN
+    int t=1;
+    cin>>t;
+    while(t--)solve();
+    return 0;
+}
